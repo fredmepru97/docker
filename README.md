dbd6159348da0abf024b99f98074330919e65cf3
fredmepru97 <fredmepru97@gmail.com>
2024-05-17 11:45:48 +0200

    Final Version

---
+++ README.md
@@ -0,0 +1,1 @@
+This exercise was done using docker, Git Bash and other useful tools with the goal to create a website that displays images, visit counters, links, menus and data analysis.  
--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 import time
 import redis
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
@@ -20,6 +21,16 @@ def get_hit_count():
 def hello():
     count = get_hit_count()
     return render_template('hello.html', name= "BIPM", count = count)
+
+titanic_data = pd.read_csv("titanic.csv")
+
+@app.route('/titanic')
+def titanic():
+    html_table = titanic_data.head().to_html()
+    titanic = titanic_data.head()
+
+    return render_template('titanic.html', titanic_preview=titanic, table=html_table)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
